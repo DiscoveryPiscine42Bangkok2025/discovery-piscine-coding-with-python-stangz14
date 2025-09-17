32994911ca387478ad3ba4e9efa22b8b14d29f2e
stangz14 <chokilover3@gmail.com>
2025-09-17 23:30:30 +0700

    add all

--- cell03/ex00/to25.py
+++ cell03/ex00/to25.py
@@ -1,4 +1,7 @@
 number = int(input("Enter a number less than 25\n"))
 
-for i in range(number, 26):
-    print(f"Inside the loop, my variable is {i}")
+if number > 25:
+    print("Error")
+else:
+    for i in range(number, 26):
+        print(f"Inside the loop, my variable is {i}")
